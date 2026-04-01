f10f731cec1a9270c5fc77f7e99c082748ad2c65
Abhay Maurya <abhaymaurya821@gmail.com>
2026-04-01 12:48:54 +0530

    feat:implementing the dark text

--- components/common/Navbar.jsx
+++ components/common/Navbar.jsx
@@ -11,33 +11,33 @@ export default function Navbar() {
   const router = useRouter();
 
   return (
-    <nav className="w-full h-16 border-b border-gray-300 bg-white shadow-sm fixed top-0 left-0 right-0 z-50">
-      <div className="flex justify-between items-center h-16 px-4 sm:px-6 lg:px-8 w-full max-w-7xl mx-auto">
+    <nav className="w-full h-16 border-b border-gray-300 bg-white shadow-sm fixed top-0 left-0 right-0 z-50 dark:bg-gray-800 dark:border-gray-700 ">
+      <div className="flex justify-between items-center h-16 px-4 sm:px-6 lg:px-8 w-full max-w-7xl mx-auto dark:text-white">
 
         {/* Logo */}
         <div className="flex items-center">
           <Link href="/" className="flex items-center gap-2">
             <Image src="/favicon.ico" alt="Logo" width={32} height={32} className="w-8 h-8" />
-            <span className="font-bold text-lg text-gray-800">AI Notes Manager</span>
+            <span className="font-bold text-lg text-gray-800 dark:text-white">AI Notes Manager</span>
           </Link>
         </div>
 
         {/* Desktop Navigation Links */}
-        <div className="hidden md:flex gap-8 items-center">
-          <Link href="#features"  className="text-gray-700 hover:text-blue-600 transition font-medium">
+        <div className="hidden md:flex gap-8 items-center text-gray-700 dark:text-white">
+          <Link href="#features"  className=" hover:text-blue-600 transition font-medium">
             Features
           </Link>
-          <Link href="/pricing" className="text-gray-700 hover:text-blue-600 transition font-medium">
+          <Link href="/pricing" className=" hover:text-blue-600 transition font-medium">
             Pricing
           </Link>
-          <Link href="/about" className="text-gray-700 hover:text-blue-600 transition font-medium">
+          <Link href="/about" className=" hover:text-blue-600 transition font-medium">
             About
           </Link>
         </div>
 
         {/* Desktop Auth Buttons */}
-        <div className="hidden md:flex gap-4 items-center">
-          <button onClick={() => router.push("/auth/login")} className="text-gray-700 hover:text-blue-600 transition font-medium cursor-pointer">
+        <div className="hidden md:flex gap-4 items-center text-gray-700 dark:text-white">
+          <button onClick={() => router.push("/auth/login")} className=" hover:text-blue-600 transition font-medium cursor-pointer">
             Sign In
           </button>
           <button onClick={() => router.push("/auth/register")} className="bg-blue-600 text-white rounded-full px-5 py-2 hover:bg-blue-700 transition font-medium cursor-pointer">
