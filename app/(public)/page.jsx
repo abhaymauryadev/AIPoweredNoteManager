81d5258891cbddb0b58fbe03b62501330dc44c3b
Abhay Maurya <abhaymaurya821@gmail.com>
2026-09-13 17:11:42 +0530

    feat: restructure HomePage component to include Navbar, Hero, Features, and Footer

--- app/(public)/page.jsx
+++ app/(public)/page.jsx
@@ -1,8 +1,16 @@
+import Hero from "@/components/sections/Hero";
+import Navbar from "@/components/common/Navbar";
+import Footer from "@/components/common/Footer";
+import Features from "@/components/sections/Features";
 export default function HomePage() {
   return (
-    <main>
-      <h1>AI Notes Manager</h1>
-      <p>Organize your thoughts with AI-powered notes.</p>
-    </main>
+    <>
+      <Navbar />
+      <section className="text-center space-y-4">
+        <Hero />
+        <Features />
+        <Footer />
+      </section>
+    </>
   );
 }
